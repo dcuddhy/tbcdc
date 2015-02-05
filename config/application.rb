515fda1526c645d23d947901dda470f63eca35ea
dcuddhy <dcuddhy@gmail.com>
2015-02-05 11:08:06 -0700

    trying to remedy heroku deploy errors - precompile assets failed

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Beards
   class Application < Rails::Application
+
+    # snatched from stack overflow to remedy heroku deploy error
+    # 'precompile assets failed'
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
