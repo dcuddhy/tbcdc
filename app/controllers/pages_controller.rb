c3a6653770b008e0ab82e3ba2d5acb3ec7d0d404
dcuddhy <dcuddhy@gmail.com>
2015-02-08 14:25:59 -0700

    made great progress in divs to the nth for video page layout.  currently see note in _video.scss ln25 to add or remove

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -74,22 +74,119 @@ class PagesController <ApplicationController
   end
 
 
+  def wob
+
+    uri = URI.parse("https://api.instagram.com/v1/tags/mustache/media/recent?access_token=43415997.30a5aa1.d9970cb87c154a77bc5745c030754a80&count=25")
+    run = (http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request))
+    @instagram = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram2 = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram2["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram3 = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram3["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram4 = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram4["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram5 = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram5["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram6 = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram6["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram7 = JSON.parse(response.body)
+
+    uri = URI.parse(@instagram7["pagination"]["next_url"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    @instagram8 = JSON.parse(response.body)
+
+    @megagram = []
+
+    @instagram['data'].each do |insta|
+      @megagram << insta
+    end
+
+    @instagram2['data'].each do |insta|
+      @megagram << insta
+    end
+
+    @instagram3['data'].each do |insta|
+      @megagram << insta
+    end
+
+    @instagram4['data'].each do |insta|
+      @megagram << insta
+    end
+
+    @wallobeard = Kaminari.paginate_array(@megagram).page(params[:page]).per(50)
+
+  end
+
+
+  def videos
+    client = Tumblr::Client.new({
+      :consumer_key => ENV['TUMBLR_CONSUMER_KEY'],
+      :consumer_secret => ENV['TUMBLR_CONSUMER_SECRET'],
+      :oauth_token => ENV['TUMBLR_OAUTH_TOKEN'],
+      :oauth_token_secret => ENV['TUMBLR_OAUTH_TOKEN_SECRET']
+    })
+
+    @videos = client.dashboard( :limit => 100, :type => 'video', :reblog_info => true )
+
+
+
+  @videos.values.each do |video|
+    @foo = Kaminari.paginate_array(video).page(params[:page]).per(9)
+  end
+
+
+
+
+  end
 
 
   def facebook
 
-    # @feeds = []
-    # @feeds << Feedjira::Feed.fetch_and_parse('https://www.facebook.com/feeds/page.php?format=rss20&id=218130979387')
-
-    # @feeds << Feedjira::Feed.fetch_and_parse('http://atlantamustache.com/feed/')
-    # @feeds << Feedjira::Feed.fetch_and_parse('http://feeds.feedburner.com/WorldBeardAndMustacheChampionships')
-    # @feeds << Feedjira::Feed.fetch_and_parse('http://beardcoach.com/feed/')
-    # @feeds << Feedjira::Feed.fetch_and_parse('http://feeds.feedburner.com/TheJacksonvilleBeardsmithSociety')
-    #
-    # entries = []
-    # @feeds.each do |feed|
-    #   entries << feed.entries
-    # end
 
     @entries = Article.order('published_at desc')
 
@@ -103,138 +200,14 @@ class PagesController <ApplicationController
   end
 
 
+  private
+    def smedia_params
 
+      params.require(:smedia).permit(:page)
 
-
-def wob
-
-  # @flickr = []
-  # @flickr << Feedjira::Feed.fetch_and_parse('http://api.flickr.com/services/feeds/photos_public.gne?tags=beard&lang=en-us&format=rss_200')
-
-  uri = URI.parse("https://api.instagram.com/v1/tags/mustache/media/recent?access_token=43415997.30a5aa1.d9970cb87c154a77bc5745c030754a80&count=25")
-  run = (http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request))
-  @instagram = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram2 = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram2["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram3 = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram3["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram4 = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram4["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram5 = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram5["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram6 = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram6["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram7 = JSON.parse(response.body)
-
-  uri = URI.parse(@instagram7["pagination"]["next_url"])
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @instagram8 = JSON.parse(response.body)
-
-  @megagram = []
-
-  @instagram['data'].each do |insta|
-    @megagram << insta
-  end
-
-  @instagram2['data'].each do |insta|
-    @megagram << insta
-  end
-
-  @instagram3['data'].each do |insta|
-    @megagram << insta
-  end
-
-  @instagram4['data'].each do |insta|
-    @megagram << insta
-  end
-
-    @wallobeard = Kaminari.paginate_array(@megagram).page(params[:page]).per(50)
-
-end
-
-
-  # entries = []
-  # @feeds.each do |feed|
-  #   entries << feed.entries
-  # end
-
-  def new
-  end
-
-  def videos
-    client = Tumblr::Client.new({
-      :consumer_key => ENV['TUMBLR_CONSUMER_KEY'],
-      :consumer_secret => ENV['TUMBLR_CONSUMER_SECRET'],
-      :oauth_token => ENV['TUMBLR_OAUTH_TOKEN'],
-      :oauth_token_secret => ENV['TUMBLR_OAUTH_TOKEN_SECRET']
-    })
-
-    @videos = client.dashboard( :limit => 100, :type => 'video', :reblog_info => true )
-
-
-    @tumblr = []
-      @videos.each do |tvideo|
-        @tumblr <<tvideo.entries
     end
 
-
-  end
-
 end
-
-private
-  def smedia_params
-
-    params.require(:smedia).permit(:page)
-
-  end
-
-
 
 #GOOGLE THIS SHIT
 #permanence / temp ~ when to use bangs
