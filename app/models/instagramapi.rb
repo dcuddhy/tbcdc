691cebde30fa87e742df163336336af6df56fe52
dcuddhy <dcuddhy@gmail.com>
2015-02-15 20:15:59 -0700

    instagram feed now points to tbcdc hashtag.  all photos from real fans

--- app/models/instagramapi.rb
+++ app/models/instagramapi.rb
@@ -3,7 +3,7 @@
   def get_instagram_posts
     megagram = []
     counter = 0
-    uri = URI.parse("https://api.instagram.com/v1/tags/mustache/media/recent?access_token=#{ENV['INSTAGRAM_ACCESS_TOKEN']}&count=25")
+    uri = URI.parse("https://api.instagram.com/v1/tags/tbcdc/media/recent?access_token=#{ENV['INSTAGRAM_ACCESS_TOKEN']}&count=25")
 
     while counter < 4
       run = (http = Net::HTTP.new(uri.host, uri.port)
